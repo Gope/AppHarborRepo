930a2cb1409ac9ed5d64339999ae3798700ac750
Gerrit Puddig <gerrit.puddig@conjin.de>
2013-06-24 14:51:00 +0200

    EmailController eingeführt

---
+++ AppHarborBuild.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AppHarborBuild.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AppHarborBuild.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bd1e84e7-f072-4f8c-8a63-1f1d68e2fc27")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AppHarborBuild/Controllers/EmailsController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AppHarborBuild.Controllers
+{
+    public class EmailsController : ApiController
+    {
+        // GET api/emails
+        public IEnumerable<Email> Get()
+        {
+            return new Email[]
+                {
+                    new Email
+                        {
+                            From = "gerrit.puddig@conjin.de",
+                            To = "paul.paulsen@conjin.de",
+                            Subject = "Hallo 1"
+                        },
+                    new Email
+                        {
+                            From = "gerrit.puddig@conjin.de",
+                            To = "paul.paulsen@conjin.de",
+                            Subject = "Hallo 2"
+                        }
+                };
+        }
+
+        // GET api/emails/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/emails
+        public void Post(Email email)
+        {
+        }
+
+        // PUT api/emails/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/emails/5
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class Email
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Subject { get; set; }
+    }
+}
